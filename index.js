79da9e90c558b3d2aa3d0d181e5e2bb9083df7b5
Jeroen Robbertsen <95624334+jeroenrobbertsen@users.noreply.github.com>
2023-03-23 10:12:42 +0000

    Make sure the maximum interested age is higher than the minimum interested age.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const mockData = require('./mockData.js').data;
 
-console.log("Welcome to the WincWinc dating app")
+// console.log("Welcome to the WincWinc dating app")
 
 const userProfile = {
   first_name: "",
@@ -15,95 +15,95 @@ const userProfile = {
   max_age_interest: "",
 }
 
-while (true) {
-  let answer = prompt("What is your First Name?");
-  if (answer === "" || answer === null) {
-    console.log("Please fill in your first name");
-  } else {
-    console.log(`Your first name is ${answer}`);
-    userProfile.first_name = answer;
-    break;
-  }
-}
+// while (true) {
+//   let answer = prompt("What is your First Name?");
+//   if (answer === "" || answer === null) {
+//     console.log("Please fill in your first name");
+//   } else {
+//     console.log(`Your first name is ${answer}`);
+//     userProfile.first_name = answer;
+//     break;
+//   }
+// }
 
-while (true) {
-  let answer = prompt("What is your Last Name?");
-  if (answer === "" || answer === null) {
-    console.log("Please fill in your last name");
-  } else {
-    console.log(`Your last name is ${answer}`);
-    userProfile.last_name = answer;
-    break;
-  }
-}
+// while (true) {
+//   let answer = prompt("What is your Last Name?");
+//   if (answer === "" || answer === null) {
+//     console.log("Please fill in your last name");
+//   } else {
+//     console.log(`Your last name is ${answer}`);
+//     userProfile.last_name = answer;
+//     break;
+//   }
+// }
 
-while (true) {
-  let answer = prompt("What is your age?");
-  if (answer === "" || answer === null) {
-    console.log("Please fill in your age");
-  } else {
-    console.log(`Your age is ${answer}`);
-    userProfile.age = Number(answer);
-    break;
-  }
-}
+// while (true) {
+//   let answer = prompt("What is your age?");
+//   if (answer === "" || answer === null) {
+//     console.log("Please fill in your age");
+//   } else {
+//     console.log(`Your age is ${answer}`);
+//     userProfile.age = Number(answer);
+//     break;
+//   }
+// }
 
-while (true) {
-  let answer = prompt("What is your gender; M, F or X?");
-  if (answer === "F") {
-    console.log("Your gender is female");
-    userProfile.gender = answer;
-    break;
-  } else if (answer === "M") {
-    console.log("Your gender is Male");
-    userProfile.gender = answer;
-    break;
-  } else if (answer === "X") {
-    console.log("Your are a transgender");
-    userProfile.gender = answer;
-    break;
-  } else {
-    console.log("Please choose between M, F or X"); answer = prompt("What is your gender?");
-    userProfile.gender = answer;
-    break;
-  }
-}
+// while (true) {
+//   let answer = prompt("What is your gender; M, F or X?");
+//   if (answer === "F") {
+//     console.log("Your gender is female");
+//     userProfile.gender = answer;
+//     break;
+//   } else if (answer === "M") {
+//     console.log("Your gender is Male");
+//     userProfile.gender = answer;
+//     break;
+//   } else if (answer === "X") {
+//     console.log("Your are a transgender");
+//     userProfile.gender = answer;
+//     break;
+//   } else {
+//     console.log("Please choose between M, F or X"); answer = prompt("What is your gender?");
+//     userProfile.gender = answer;
+//     break;
+//   }
+// }
 
-while (true) {
-  let answer = prompt("What gender are you interested in; M, F, X or B?");
-  if (answer === "F") {
-    console.log("You are interested in dating a female");
-    userProfile.gender_interest = answer;
-    break;
-  } else if (answer === "M") {
-    console.log("You are interested in dating a male");
-    userProfile.gender_interest = answer;
-    break;
-  } else if (answer === "X") {
-    console.log("You are interested in dating a transgender");
-    userProfile.gender_interest = answer;
-    break;
-  } else if (answer === "B") {
-    console.log("You are interested in dating a male and female");
-    userProfile.gender_interest = answer;
-    break;
-  } else {
-    console.log("Please choose between M, F, X or B"); answer = prompt("What is your gender?");
-    userProfile.gender = answer;
-    break;
-  }
-}
+// while (true) {
+//   let answer = prompt("What gender are you interested in; M, F, X or B?");
+//   if (answer === "F") {
+//     console.log("You are interested in dating a female");
+//     userProfile.gender_interest = answer;
+//     break;
+//   } else if (answer === "M") {
+//     console.log("You are interested in dating a male");
+//     userProfile.gender_interest = answer;
+//     break;
+//   } else if (answer === "X") {
+//     console.log("You are interested in dating a transgender");
+//     userProfile.gender_interest = answer;
+//     break;
+//   } else if (answer === "B") {
+//     console.log("You are interested in dating a male and female");
+//     userProfile.gender_interest = answer;
+//     break;
+//   } else {
+//     console.log("Please choose between M, F, X or B"); answer = prompt("What is your gender?");
+//     userProfile.gender = answer;
+//     break;
+//   }
+// }
 
-while(true) {
-  let answer = prompt("Where are you located; city or rural?");
-  if (answer === "" || answer === null) {
-    console.log("Please choose city or rural");
-  } else {
-    console.log(`Your live in a ${answer} area`);
-    userProfile.location = answer;
-    break;
-  }
-}
+// while(true) {
+//   let answer = prompt("Where are you located; city or rural?");
+//   if (answer === "" || answer === null) {
+//     console.log("Please choose city or rural");
+//   } else {
+//     console.log(`Your live in a ${answer} area`);
+//     userProfile.location = answer;
+//     break;
+//   }
+// }
 
 while(true) {
   let answer = prompt("What is the minimal age of your match candidate?");
@@ -112,7 +112,7 @@ while(true) {
   } else {
     console.log(`You are interested in a candidate with the minimal age of ${answer}`);
     userProfile.min_age_interest = Number(answer);
-    break;jeroen
+    break;
     
   }
 }
@@ -121,6 +121,8 @@ while(true) {
   let answer = prompt("What is the maximal age of your match candidate?");
   if (answer <= 18) {
     console.log("The maximal age is 18, please choose again");
+  } else if (answer < userProfile.min_age_interest) {
+    console.log("The maximal age interest is lower than the minimal age, please choose again ");
   } else {
     console.log(`You are interested in a candidate with the maximal age of ${answer}`);
     userProfile.max_age_interest = Number(answer);
